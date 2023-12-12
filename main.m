c657cd78300b94ce59433c528bc50d6c6127ba1a
Hyrum-Taylor <82358970+Hyrum-Taylor@users.noreply.github.com>
2023-12-11 17:23:14 -0700

    variable units

--- main.m
+++ main.m
@@ -4,13 +4,13 @@
 
 %% Variables
 
-x0 = 1; % initial x position
-vx0 = 0; % initial x velocity
-y0 = 0; % initial y position
-vy0 = 0; % initial y velocity
-g = -9.8; % gravity
-L = 100; % length of pendulum string
-lat = .7; % lattitude (approx for Provo) (equator is 0, poles are pi/2)
-rot_earth = 2*pi/(60*60*24); % angular speed of earth
+x0 = 1; % initial x position (m)
+vx0 = 0; % initial x velocity (m/s)
+y0 = 0; % initial y position (m)
+vy0 = 0; % initial y velocity (m/s)
+g = -9.8; % gravity (m/s^2)
+L = 100; % length of pendulum string (m)
+lat = .7; % lattitude (approx for Provo) (equator is 0, poles are pi/2) (rad)
+rot_earth = 2*pi/(60*60*24); % angular speed of earth (rad/sec)
 
 %% Function calls

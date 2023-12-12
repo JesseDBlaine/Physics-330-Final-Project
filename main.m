c1511753fd0654c7efc5efced28f2a807ae7f28e
Hyrum-Taylor <82358970+Hyrum-Taylor@users.noreply.github.com>
2023-12-11 17:20:01 -0700

    variables

--- main.m
+++ main.m
@@ -2,4 +2,15 @@
 % Physics 330 Final Project
 % Foucault Pendulum Simulator
 
-%%
+%% Variables
+
+x0 = 1; % initial x position
+vx0 = 0; % initial x velocity
+y0 = 0; % initial y position
+vy0 = 0; % initial y velocity
+g = -9.8; % gravity
+L = 100; % length of pendulum string
+lat = .7; % lattitude (approx for Provo) (equator is 0, poles are pi/2)
+rot_earth = 2*pi/(60*60*24); % angular speed of earth
+
+%% Function calls

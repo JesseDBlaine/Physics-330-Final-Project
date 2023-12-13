dda11d1cc7b4a8c35c88cf613fbc944ec980ea3b
JesseDBlaine <137823874+JesseDBlaine@users.noreply.github.com>
2023-12-13 12:16:41 -0700

    Interpolation
    Attempted Interpolation, failed with error that mentions index number exceeding array number. I think the issue is z. Not sure

--- main.m
+++ main.m
@@ -23,9 +23,9 @@ rot_earth = 2*pi/(60*60*24); % angular speed of earth (rad/sec)
 %Initial values matrix
 u0 = zeros(4,1);
 
-u0(1) = 0; %x0
+u0(1) = 1; %x0
 u0(2) = 0; %y0
-u0(3) = 1; %vx0
+u0(3) = 0; %vx0
 u0(4) = 0; %vy0
 
 
@@ -33,8 +33,13 @@ u0(4) = 0; %vy0
 
 options = odeset('RelTol',1e-5);
 
+%time
+N = 2^14;
+dt = 1000/N;
+time = 0:dt:1000;
+
 %Solve
-ode45(@focP,0:.1:1000,u0,options)
+[t,u] = ode45(@focP,time,u0,options);
 
 %Unload
 x = u(1);
@@ -47,28 +52,36 @@ vy = u(4);
 
 close all;
 
-dt=.1;
-time_watching = 100;
-t = 0:dt:time_watching;
-% x = interpn(,,,'spline'); % for when we get the ODE45 data
+% dt=.1;
+% time_watching = 100;
+% % t = 0:dt:time_watching;
 
-theta = .01*cos(t); % These are temporary for working on the graphing feature
-phi = .01*sin(t);
+M = 5*length(t);
+tau = t(end)/M;
+te = tau*(0:M-1);
+xe = interp1(t, u(:,1), te, 'spline', 'extrap');
+ye = interp1(t, u(:,2), te, 'spline','extrap');
+% xe = interpn(,,,'spline'); % for when we get the ODE45 data
 
-x = L*sin(theta).*cos(phi); % transform from spherical to cartesian
-y = L*sin(theta).*sin(phi);
+% theta = .01*cos(t); % These are temporary for working on the graphing feature
+% phi = .01*sin(t);
+
+% x = L*sin(theta).*cos(phi); % transform from spherical to cartesian
+% y = L*sin(theta).*sin(phi);
 z = L*(1-cos(theta));
 
 figure; % plot the path traced by the pendulum
-plot(x,y,'b-')
+plot(xe,ye,'b-')
 xlabel("X")
 ylabel("Y")
 title("Path traced by pendulum")
 grid on;
 
+%Keep getting error, wondering if the issues is z
+
 figure;
 for k = 1:length(t) % animation for the pendulum swinging
-    plot3([0,x(k)],[0,y(k)],[L,z(k)], x(k),y(k),z(k), 'square')
+    plot3([0,xe(k)],[0,ye(k)],[L,z(k)], xe(k),ye(k),z(k), 'square')
     xlim([-2,2])
     ylim([-2 2])
     zlim([0 100])
